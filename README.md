fd4d5d20c9a41c3fa7f15a09be2cc0f2f0236a92
julien-lunii <julien@lunii.fr>
2021-08-20 18:08:10 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+#BBCode image
+
+This NodeBB plugin allows you to use BBCode in post to share image.
+
+The image is updated automatically in the composer preview.
+
+You can use this syntax commonly given by image hosts :
+```
+[url=http://someurl.com/][img]http://someurl.com/someimage.png[/img][/url]
+```
---
+++ library.js
@@ -0,0 +1,56 @@
+function replaceContent(data) {
+
+    imgRegex = /\[img\](.*?)\[\/img\]/g;
+    urlRegex = /\[url=(.*?)\]/g;
+
+    var newData = data;
+
+    newData = newData.replace(imgRegex, (match, src) => {
+
+        var url;
+
+        src.replace(/<a href=\"(.*?)\"/g, (match2, src2) => {
+            url = src2;
+        });
+
+        url = url.split("%5B/img%5D").join("");
+        url = url.split("%5B/url%5D").join("");
+
+        return "<img src=\"" + url + "\"</img>";
+    });
+
+    newData = newData.replace(urlRegex, (match, src) => {
+        return "<a href=\"" + src + "\">";
+    });
+
+    newData = newData.replace("[/url]", "</a>");
+
+    return newData;
+}
+
+
+var ImageBBCode = {
+
+    parseBBCodeImages: function(data, callback) {
+
+        if (data && data.postData && data.postData.content) {
+
+            data.postData.content = replaceContent(data.postData.content);
+        }
+
+
+        callback(null, data);
+    },
+
+    parseBBCodeImagesInComposerPreview: function(data, callback) {
+
+        if (data) {
+
+            data = replaceContent(data);
+        }
+
+        callback(null, data);
+    }
+};
+
+module.exports = ImageBBCode;
